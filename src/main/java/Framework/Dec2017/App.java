71583b709b97311b6b3212d69b1e1eac48a165ab
nirmalaramalingam <nirmalar18@gmail.com>
2017-12-19 12:07:28 +0530

    first commit

---
+++ src/main/java/Framework/Dec2017/App.java
@@ -0,0 +1,13 @@
+package Framework.Dec2017;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
